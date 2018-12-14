581596bd3aab990bffe7de4f65f21adb1afa6f41
maniek <mpastuszka@gmail.com>
2018-12-14 12:08:16 +0100

    [init] - initial commit

---
+++ src/main/java/pl/mpas/anotherspringdemo/AnotherSpringDemoApplication.java
@@ -0,0 +1,14 @@
+package pl.mpas.anotherspringdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnotherSpringDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnotherSpringDemoApplication.class, args);
+    }
+
+}
+
