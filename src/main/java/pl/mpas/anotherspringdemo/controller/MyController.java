e73fe762efe5e071ebac4ca24b30ed3a0b6695ee
maniek <mpastuszka@gmail.com>
2018-12-14 13:08:36 +0100

    [change] - new endpoint

--- src/main/java/pl/mpas/anotherspringdemo/controller/MyController.java
+++ src/main/java/pl/mpas/anotherspringdemo/controller/MyController.java
@@ -1,6 +1,7 @@
 package pl.mpas.anotherspringdemo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -9,5 +10,14 @@ public class MyController {
     @GetMapping({"/", "/home"})
     public String welcome() {
         return "welcome";
+    }
+
+    @GetMapping("/me")
+    public String me(Model model) {
+
+        model.addAttribute("name", "Mariusz");
+        model.addAttribute("surname", "P.");
+
+        return "me";
     }
 }
