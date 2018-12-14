50e0c64b51250ea07eb6803c74809d8b84e273ee
maniek <mpastuszka@gmail.com>
2018-12-14 14:08:21 +0100

    [add] - car endpoint

--- src/main/java/pl/mpas/anotherspringdemo/controller/MyController.java
+++ src/main/java/pl/mpas/anotherspringdemo/controller/MyController.java
@@ -1,6 +1,7 @@
 package pl.mpas.anotherspringdemo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -9,5 +10,14 @@ public class MyController {
     @GetMapping({"/", "/home"})
     public String welcome() {
         return "welcome";
+    }
+
+    @GetMapping("/car")
+    public String myFavoriteCar(Model model) {
+
+        model.addAttribute("brand", "Saab");
+        model.addAttribute("model", "93");
+
+        return "car";
     }
 }
