9dd2eeaa51db85598725ddd793786da187dba369
maniek <mpastuszka@gmail.com>
2018-12-14 12:20:00 +0100

    [add] - first controller

---
+++ src/main/java/pl/mpas/anotherspringdemo/controller/MyController.java
@@ -0,0 +1,13 @@
+package pl.mpas.anotherspringdemo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MyController {
+
+    @GetMapping({"/", "/home"})
+    public String welcome() {
+        return "welcome";
+    }
+}
